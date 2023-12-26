a2ebbc0ebce714f6e3afdf008df1f0adaaa25ddc
frostybee <rsleiman2@gmail.com>
2023-12-26 17:12:45 -0500

    refactor: Change the loading of hosted services.

--- src/FriskyMouse/App.xaml.cs
+++ src/FriskyMouse/App.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace FriskyMouse;
+﻿using System.Reflection;
+
+namespace FriskyMouse;
 
 /// <summary>
 /// Interaction logic for App.xaml
@@ -6,66 +8,44 @@
 public partial class App : Application
 {
     #region Fields
-    public static readonly AppConfigurationInfo Configuration = new AppConfigurationInfo();       
+    public static readonly AppConfigurationInfo Configuration = new AppConfigurationInfo();
     /// <summary>
     /// A named system-wide mutex used to ensure that only one instance of this application runs at once. 
     /// </summary>
     private static string _mutexName = "{FFF0FDB8-C9C3-4B9B-8CE5-92BFD1D8E17F}";
     private Mutex _mutex;
     private bool _singleInstanceClose = false;
+    private IHost _host;
     #endregion
 
-    // The .NET Generic Host provides dependency injection, configuration, logging, and other services.
-    // https://docs.microsoft.com/dotnet/core/extensions/generic-host
-    // https://docs.microsoft.com/dotnet/core/extensions/dependency-injection
-    // https://docs.microsoft.com/dotnet/core/extensions/configuration
-    // https://docs.microsoft.com/dotnet/core/extensions/logging
-    private static readonly IHost _host = Host.CreateDefaultBuilder()
-        .ConfigureAppConfiguration(c =>
-        {
-            c.SetBasePath(AppContext.BaseDirectory);
-        })
-        .ConfigureServices(
-            (context, services) =>
-            {
-                // App Host
-                services.AddHostedService<ApplicationHostService>();
-
-                // Main window container with navigation
-                services.AddSingleton<IWindow, MainWindow>();
-                services.AddSingleton<MainWindowViewModel>();
-                services.AddSingleton<INavigationService, NavigationService>();
-                services.AddSingleton<ISnackbarService, SnackbarService>();
-                services.AddSingleton<IContentDialogService, ContentDialogService>();
-                services.AddSingleton<WindowsProviderService>();
-
-                // Top-level view pages and their view models. 
-                services.AddSingleton<DashboardPage>();
-                services.AddSingleton<DashboardViewModel>();
-                services.AddSingleton<SpotlightPage>();
-                services.AddSingleton<SpotlightViewModel>();
-                services.AddSingleton<ClickIndicatorPage>();
-                services.AddSingleton<ClickIndicatorViewModel>();
-                services.AddSingleton<RightClickEffectPage>();
-                services.AddSingleton<RightClickEffectViewModel>();
-                services.AddSingleton<SettingsPage>();
-                services.AddSingleton<SettingsViewModel>();
-                services.AddSingleton<UiTestsPage>();
-                services.AddSingleton<UiTestsViewModel>();
-
-            }
-        )
-        .Build();
 
 
     /// <summary>
     /// Occurs when the application is loading.
     /// </summary>
-    private void OnApplication_Startup(object sender, StartupEventArgs e)
+    private async void OnApplication_Startup(object sender, StartupEventArgs e)
     {
         _mutex = new Mutex(true, _mutexName);
         var mutexIsAcquired = _mutex.WaitOne(TimeSpan.Zero, true);
-        if (!mutexIsAcquired)
+        if (mutexIsAcquired)
+        {
+            // No running instance has been detected,
+            // We Start a new instance of this application.
+            _mutex.ReleaseMutex();
+
+            _host = Host.CreateDefaultBuilder(e.Args)
+                .ConfigureAppConfiguration(c =>
+                {
+                    c.SetBasePath(AppContext.BaseDirectory);
+                })
+                .ConfigureServices(ConfigureServices)
+                .Build();
+            //_host.Start();
+            await _host.StartAsync();
+            LoadAppConfigurationInfo();
+            SettingsManager.LoadSettings();
+        }
+        else
         {
             // An instance of this application is already running.
             // Bring it into the foreground
@@ -74,16 +54,71 @@ public partial class App : Application
             _singleInstanceClose = true;
             Shutdown();
         }
-        else
-        {
-            // No running instance has been detected,
-            // We Start a new instance of this application.
-            _mutex.ReleaseMutex();            
-            _host.Start();
-            LoadAppConfigurationInfo();
-            SettingsManager.LoadSettings();
-            AppConfigurationManager.SayHello();            
-        }        
+    }
+    private void ConfigureServices(HostBuilderContext context, IServiceCollection services)
+    {
+
+        // The .NET Generic Host provides dependency injection, configuration, logging, and other services.
+        // https://docs.microsoft.com/dotnet/core/extensions/generic-host
+        // https://docs.microsoft.com/dotnet/core/extensions/dependency-injection
+        // https://docs.microsoft.com/dotnet/core/extensions/configuration
+        // https://docs.microsoft.com/dotnet/core/extensions/logging
+
+        // TODO: Register services, viewmodels and pages here.
+
+        // App Host
+        services.AddHostedService<ApplicationHostService>();
+
+        // App services. 
+        services.AddSingleton<INavigationService, NavigationService>();
+        services.AddSingleton<ISnackbarService, SnackbarService>();
+        services.AddSingleton<IContentDialogService, ContentDialogService>();
+
+        // Main window container with navigation
+        services.AddSingleton<IWindow, MainWindow>();
+        services.AddSingleton<MainWindowViewModel>();
+
+        // Views and their ViewModels. 
+        services.AddSingleton<DashboardPage>();
+        services.AddSingleton<DashboardViewModel>();
+        services.AddSingleton<SpotlightPage>();
+        services.AddSingleton<SpotlightViewModel>();
+        services.AddSingleton<ClickIndicatorPage>();
+        services.AddSingleton<ClickIndicatorViewModel>();
+        services.AddSingleton<RightClickEffectPage>();
+        services.AddSingleton<RightClickEffectViewModel>();
+        services.AddSingleton<SettingsPage>();
+        services.AddSingleton<SettingsViewModel>();
+        services.AddSingleton<UiTestsPage>();
+        services.AddSingleton<UiTestsViewModel>();
+
+    }
+
+    /// <summary>
+    /// Occurs when the application is closing.
+    /// </summary>
+    private async void OnExit(object sender, ExitEventArgs e)
+    {
+        await _host.StopAsync();
+        _host.Dispose();
+        _host = null;
+
+        // Uninstall the global mouse hook.
+        DecorationManager.Instance?.DisableHook();
+        DecorationManager.Instance?.Dispose();
+        SettingsManager.SaveSettings();
+        Console.WriteLine("Exiting the application...");
+    }
+
+    /// <summary>
+    /// Occurs when an exception is thrown by an application but not handled.
+    /// </summary>
+    private void OnDispatcherUnhandledException(
+        object sender,
+        DispatcherUnhandledExceptionEventArgs e
+    )
+    {
+        // For more info see https://docs.microsoft.com/en-us/dotnet/api/system.windows.application.dispatcherunhandledexception?view=windowsdesktop-6.0
     }
 
     private void LoadAppConfigurationInfo()
@@ -103,38 +138,14 @@ public partial class App : Application
 #endif
     }
 
+
     /// <summary>
     /// Gets registered service.
     /// </summary>
     /// <typeparam name="T">Type of the service to get.</typeparam>
     /// <returns>Instance of the service or <see langword="null"/>.</returns>
-    public static T? GetService<T>() where T : class
+    public T GetService<T>() where T : class
     {
         return _host.Services.GetService(typeof(T)) as T ?? null;
-    }
-
-    /// <summary>
-    /// Occurs when the application is closing.
-    /// </summary>
-    private void OnExit(object sender, ExitEventArgs e)
-    {
-        _host.StopAsync().Wait();
-        _host.Dispose();
-        // Uninstall the gloabl mouse hook.
-        DecorationManager.Instance?.DisableHook();
-        DecorationManager.Instance?.Dispose();
-        SettingsManager.SaveSettings();
-        Console.WriteLine("Exiting the application...");
-    }
-
-    /// <summary>
-    /// Occurs when an exception is thrown by an application but not handled.
-    /// </summary>
-    private void OnDispatcherUnhandledException(
-        object sender,
-        DispatcherUnhandledExceptionEventArgs e
-    )
-    {
-        // For more info see https://docs.microsoft.com/en-us/dotnet/api/system.windows.application.dispatcherunhandledexception?view=windowsdesktop-6.0
     }
 }
