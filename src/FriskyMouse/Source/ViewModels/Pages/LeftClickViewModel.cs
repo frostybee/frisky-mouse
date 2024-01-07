44d3f2a7832ad1d8abd8e2b0c437c1207374d3e2
frostybee <rsleiman2@gmail.com>
2024-01-07 05:51:18 -0500

    feat: Add full implementation of the mouse left click indicator.

--- src/FriskyMouse/Source/ViewModels/Pages/LeftClickViewModel.cs
+++ src/FriskyMouse/Source/ViewModels/Pages/LeftClickViewModel.cs
@@ -1,10 +1,18 @@
 ﻿
 
+
+using FriskyMouse.Drawing;
+using FriskyMouse.Drawing.Attributes;
+using FriskyMouse.Drawing.Extensions;
+using System.Runtime;
+
 namespace FriskyMouse.ViewModels.Pages;
 public partial class LeftClickViewModel : ObservableObject, INavigationAware
 {
     #region Fields
-    [ObservableProperty]
+    private ClickEffectController _profileManager;
+    private BaseRippleProfile _currentProfile;
+
     private RippleProfileInfo _leftClickOptions;    
     
     [ObservableProperty]
@@ -42,7 +50,7 @@ public partial class LeftClickViewModel : ObservableObject, INavigationAware
     private AnimationDirection _animationDirection;
 
     [ObservableProperty]
-    private double _animationSpeed;
+    private int _animationSpeed;
     #endregion
 
     #region Visual Appearance
@@ -76,74 +84,130 @@ public partial class LeftClickViewModel : ObservableObject, INavigationAware
     private void InitializeViewModel()
     {
         _isInitialized = true;
-        LeftClickOptions = SettingsManager.Current.LeftClickProperties;
-        RippleFillColor = LeftClickOptions.FillColor.ToMediaColor();
-        // Load the list of ripple profiles from their corresponding enum.
-        RippleProfiles = FMAppHelper.GetEnumDescriptions<RippleProfileType>();
-        SelectedRippleProfile = (int)LeftClickOptions.CurrentRippleProfile;
+        _profileManager = DecorationManager.Instance.ClickDecorator;
+        _leftClickOptions = SettingsManager.Current.LeftClickProperties;
+        _currentProfile = new FilledSonarPulseProfile();
+
+        RippleFillColor = _leftClickOptions.FillColor.ToMediaColor();
+        //
+        LoadRipplesProfile();        
 
         // Load the list of animation directions from their corresponding enum.
         AnimationDirections = FMAppHelper.GetEnumDescriptions<AnimationDirection>();
-        SelectedAnimationDirection = (int)LeftClickOptions.AnimationDirection;
-
+        SelectedAnimationDirection = (int)_leftClickOptions.AnimationDirection;
+        
         // Load the list of animation's easing functions from their corresponding enum.
         Interpolators = FMAppHelper.GetEnumDescriptions<InterpolationType>();
-        SelectedInterpolator = (int)LeftClickOptions.InterpolationType;
+        SelectedInterpolator = (int)_leftClickOptions.InterpolationType;
         LoadLeftClickOptions();
+    }
+
+    private void LoadRipplesProfile()
+    {
+        // Load the list of ripple profiles from their corresponding enum.
+        RippleProfiles = FMAppHelper.GetEnumDescriptions<RippleProfileType>();
+        SelectedRippleProfile = (int)_leftClickOptions.CurrentRippleProfile;
+        SwitchRippleProfile((RippleProfileType)SelectedRippleProfile);
+        AdjustAnimationSpeed((int)(_leftClickOptions.AnimationSpeed * 1000));
+    }
+
+    private void AdjustAnimationSpeed(int speed)
+    {
+        //_leftClickOptions.AnimationSpeed = speed;
+        // Increase the animation speed.
+        double speedRate = (double)speed / 1000d;
+        //_rippleValueAnimator.Increment = speedRate;
+        _leftClickOptions.AnimationSpeed = speedRate;        
+        AnimationSpeed = speed;
+        _profileManager.ApplySettings(_leftClickOptions);
+    }
+
+    private void SwitchRippleProfile(RippleProfileType profileType)
+    {
+        BaseRippleProfile _newProfile = ConstructableFactory.GetInstanceOf<BaseRippleProfile>(profileType);
+        _currentProfile?.Dispose();
+        _currentProfile = _newProfile;
+        _profileManager.SwitchProfile(_newProfile);
+        _leftClickOptions.CurrentRippleProfile = profileType;
+        _currentProfile.UpdateRipplesStyle(_leftClickOptions);
     }
 
     private void LoadLeftClickOptions()
     {
-        IsEnabled = LeftClickOptions.IsEnabled;
-        CanFadeColor = LeftClickOptions.CanFadeColor;
-        InitialOpacity = LeftClickOptions.InitialOpacity;
-        RadiusMultiplier = LeftClickOptions.RadiusMultiplier;
-        OpacityMultiplier = LeftClickOptions.OpacityMultiplier;
+        IsEnabled = _leftClickOptions.IsEnabled;
+        CanFadeColor = _leftClickOptions.CanFadeColor;
+        InitialOpacity = _leftClickOptions.InitialOpacity;
+        RadiusMultiplier = _leftClickOptions.RadiusMultiplier;
+        OpacityMultiplier = _leftClickOptions.OpacityMultiplier;
     }
 
+    private void SwitchAnimationInterpolator(InterpolationType interpolator)
+    {
+        // The animation's saved easing mode has been changed.                                     
+        //InterpolationType interpolation = cmbInterpolationMode.GetSelectedEnumValue<InterpolationType>();
+        //_rippleValueAnimator.Interpolation = interpolation;
+        //_settings.InterpolationType = interpolation;
+        _profileManager.ApplySettings(_leftClickOptions);        
+        // Adjust the animation speed based on the recommended value associated with the selected 
+        // savedEasing mode. 
+        DefaultSpeedAttribute speedAttribute = interpolator.GetEnumAttribute<DefaultSpeedAttribute>();
+        AdjustAnimationSpeed(speedAttribute.Speed);
+
+        // TODO: Display the recommended speed in a label instead.
+        //sliderAnimSpeed.Value = speedAttribute.Speed;
+        //StartAnimation();
+    }
     partial void OnIsEnabledChanged(bool value)
     {
-        LeftClickOptions.IsEnabled = value;
+        _leftClickOptions.IsEnabled = value;
     }
 
     partial void OnCanFadeColorChanged(bool value)
     {
-        LeftClickOptions.CanFadeColor = value;
+        _leftClickOptions.CanFadeColor = value;
     }
 
     partial void OnInitialOpacityChanged(ushort value)
     {
-        LeftClickOptions.InitialOpacity = value;
+        _leftClickOptions.InitialOpacity = value;
     }
 
     partial void OnRadiusMultiplierChanged(ushort value)
     {
-        LeftClickOptions.RadiusMultiplier = value;
+        _leftClickOptions.RadiusMultiplier = value;
     }
 
     partial void OnOpacityMultiplierChanged(ushort value)
     {
-        LeftClickOptions.OpacityMultiplier = value;
+        _leftClickOptions.OpacityMultiplier = value;
     }
 
     partial void OnSelectedRippleProfileChanged(int value)
     {
-        LeftClickOptions.CurrentRippleProfile = (RippleProfileType)value;        
+        _leftClickOptions.CurrentRippleProfile = (RippleProfileType)value;
+        SwitchRippleProfile((RippleProfileType)value);
+    }
+
+    partial void OnAnimationSpeedChanged(int value)
+    {
+        AdjustAnimationSpeed(value);
     }
 
     partial void OnSelectedAnimationDirectionChanged(int value)
     {
-        LeftClickOptions.AnimationDirection = (AnimationDirection)value;
+        _leftClickOptions.AnimationDirection = (AnimationDirection)value;
     }
 
     partial void OnSelectedInterpolatorChanged(int value)
     {
-        LeftClickOptions.InterpolationType = (InterpolationType)value;
-    }
+        _leftClickOptions.InterpolationType = (InterpolationType)value;
+        SwitchAnimationInterpolator((InterpolationType)value);
+    }   
 
     partial void OnRippleFillColorChanged(System.Windows.Media.Color value)
     {
-        LeftClickOptions.FillColor = value.ToDrawingColor();
-        Console.WriteLine(value.ToString());
+        _leftClickOptions.FillColor = value.ToDrawingColor();
+        _currentProfile?.UpdateRipplesStyle(_leftClickOptions);
+        //Console.WriteLine(value.ToString());
     }
 }
