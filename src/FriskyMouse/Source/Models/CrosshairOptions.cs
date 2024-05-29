928288d2461794a2cc11cd3b0d51b1887150410c
frostybee <rsleiman2@gmail.com>
2024-05-29 01:24:05 -0400

    chore: update UI lables

--- src/FriskyMouse/Source/Models/CrosshairOptions.cs
+++ src/FriskyMouse/Source/Models/CrosshairOptions.cs
@@ -8,9 +8,9 @@ public class CrosshairOptions
     public bool IsEnabled { get; set; } = true;    
     public Color LineColor { get; set; } = Color.Red;    
     public int LineWidth { get; set; } = 2;    
-    public int Length { get; set; } = 15;
+    public int Length { get; set; } = 20;
     public SpotlightOutlineTypes OutlineStyle { get; set; } = SpotlightOutlineTypes.Dash;
-    public LineCapTypes LineCapStyle { get; set; } = LineCapTypes.ArrowAnchor;
+    public LineCapTypes LineCapStyle { get; set; } = LineCapTypes.None;
     public byte OpacityPercentage { get; set; } = 95;
     public byte Opacity
     {
